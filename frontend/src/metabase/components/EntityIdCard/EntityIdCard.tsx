e32755ef021bd35ab4d5d142d006b442b7ece17e
Raphael Krut-Landau <raphael.kl@gmail.com>
2024-09-27 15:55:22 -0400

    Remove custom code for making the entityid info popover open on enter, since we'll get that with Mantine v7

--- frontend/src/metabase/components/EntityIdCard/EntityIdCard.tsx
+++ frontend/src/metabase/components/EntityIdCard/EntityIdCard.tsx
@@ -1,9 +1,7 @@
-import { useRef, useState } from "react";
 import { t } from "ttag";
 
 import { SidesheetCard } from "metabase/common/components/Sidesheet";
 import { useDocsUrl } from "metabase/common/hooks";
-import { useCallOnEnter } from "metabase/common/utils/keyboard";
 import { CopyButton } from "metabase/components/CopyButton";
 import Link from "metabase/core/components/Link";
 import { Flex, Group, Icon, Paper, Popover, Text } from "metabase/ui";
@@ -14,34 +12,16 @@ const EntityIdTitle = () => {
   const { url: docsLink, showMetabaseLinks } = useDocsUrl(
     "installation-and-operation/serialization",
   );
-  const [infoPopoverOpen, setInfoPopoverOpen] = useState(false);
-
-  const openPopoverOnEnter = useCallOnEnter(() =>
-    setInfoPopoverOpen(open => !open),
-  );
-
-  const dropdownRef = useRef<HTMLDivElement>(null);
 
   return (
     <Group spacing="sm">
       {t`Entity ID`}
-      <Popover position="top-start" opened={infoPopoverOpen}>
+      <Popover position="top-start">
         <Popover.Target>
-          <Icon
-            onClick={() => setInfoPopoverOpen(open => !open)}
-            onBlur={e => {
-              if (!dropdownRef.current?.contains(e.relatedTarget)) {
-                setInfoPopoverOpen(false);
-              }
-            }}
-            tabIndex={0}
-            name="info"
-            className={Styles.InfoIcon}
-            onKeyDown={openPopoverOnEnter}
-          />
+          <Icon tabIndex={0} name="info" className={Styles.InfoIcon} />
         </Popover.Target>
         <Popover.Dropdown>
-          <Paper p="md" maw="13rem" ref={dropdownRef}>
+          <Paper p="md" maw="13rem">
             <Text size="sm">
               {t`When using serialization, replace the sequential ID with this global entity ID to have stable URLs across environments. Also useful when troubleshooting serialization.`}{" "}
               {showMetabaseLinks && (
